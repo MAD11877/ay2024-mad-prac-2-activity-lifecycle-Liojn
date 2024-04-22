3496b20b704869b2c819fafbe76230e6f7adc76a
Liojn <s10259865@connect.np.edu.sg>
2024-04-22 23:53:15 +0800

    Complete

--- app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.widget.Button;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-}
+
+
+    User user = new User("John Doe", "MAD Developer", 1, false);
+    TextView tvName = findViewById(R.id.textView2);
+    TextView tvDescription = findViewById(R.id.textView3);
+
+    Button btnFollow = findViewById(R.id.button1);
+
+
+    tvName.setText(user.name);
+    tvDescription.setText(user.description);
+    btnFollow.setText("Follow");
+}}
